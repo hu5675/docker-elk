756f534445c451acab8129f2554469e0c18187ea
huping <hu5675@126.com>
2019-03-14 14:34:28 +0800

    简单修改下，方便公司演示写文档

--- start.sh
+++ start.sh
@@ -6,11 +6,11 @@ chmod 777 -R data
 docker network create net-es
 docker-compose up -d
 
-echo  "wait one minute."
-sleep 1m		# 睡眠一分钟等待es启动成功
+# echo  "wait one minute."
+# sleep 1m		# 睡眠一分钟等待es启动成功
 
-echo  "setup elastic password: elastic, kibana password:kibana,logstash_system password:logstash,beats_system password:filebeat "
-docker exec -it es bash ./bin/elasticsearch-setup-passwords interactive
+# echo  "setup elastic password: elastic, kibana password:kibana,logstash_system password:logstash,beats_system password:filebeat "
+# docker exec -it es bash ./bin/elasticsearch-setup-passwords interactive
 
 # es-head
 
